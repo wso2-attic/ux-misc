c154aa45ee589f809cb409c8ff022819811a9445
thusithakalugamage <thusitha32@yahoo.com>
2016-02-17 00:55:51 +0530

    updating new styles and pages

--- wso2-con-analytics/js/main.js
+++ wso2-con-analytics/js/main.js
@@ -193,6 +193,64 @@
 		});
 	};
 
+	var checkCEPStatus = function(){
+		//var ws = new WebSocket("ws://10.100.4.2:9763/outputwebsocket/WebSocket");
+		
+		var ws = new WebSocket("ws://10.100.4.185:9766/outputwebsocket/mySocket");
+		ws.onmessage = function(e){
+			var msg = JSON.parse(e.data);
+			if(msg.event.payloadData.name == "FINSH" && $('.speech-stat-container').length == 0){
+
+
+
+			var element = "<li class='speech-stat-container'>
+		            <div class='fh5co-cover fh5co-cover-style-2 js-full-height' data-stellar-background-ratio='0.5' data-next='yes'>
+		                <div class='fh5co-overlay'></div>
+		                <div class='fh5co-cover-text'>
+		                    <div class='container-fluid'>
+		                        <div class='row'>
+		                            <div class='col-md-12 full-height js-full-height'>
+		                                <div class='fh5co-cover-intro'>
+		                                <h1 class='cover-text-lead wow fadeInUp' data-wow-duration='1s' data-wow-delay='.5s'>Speech Stats</h1><h5 class='wow fadeInUp beta-tag' data-wow-duration='1s' data-wow-delay='.9s'>Beta</h5>
+		                                <div class='graph-container speech-stat'>
+		                                    <img class='preserve' src='images/doug.jpg'/>
+		                                    <div class='stat-container'>
+		                                        <h2>You just listened to <span class='speaker-name'>Kasun Gajasinghe</span></h2>
+		                                        <h3>Building Services with WSO2 Application Server and WSO2 Microservices Framework for Java</h3>
+		                                        <p >Kasun is a product lead of WSO2 Application Server and the WSO2 Carbon Kernel, which provides the core functionality for the WSO2 middleware stack. He is a key founding member of the Siddhi Complex Event Processing Engine which is now shipped with the WSO2 CEP server. He is an active committer of DocBook project, and Gentoo Linux. As an open source advocate, he has helped to raise awareness of open source in Sri Lanka and has mentored university students.</p>
+		                                        <div class='word-count-container'>
+		                                            <div class='word-container'>
+		                                                <div class='word-count'>
+		                                                    <h2 class='count'>33 </h2>
+		                                                    <span>times</span>
+		                                                    <div class='clearfix'></div>
+		                                                </div>
+		                                                <h3 class='word'>Carbon Server</h3>
+		                                            </div>
+		                                        </div>
+		                                    </div>
+		                                </div>
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </li>"
+
+				$('.slides-container').append(element);
+				$('#page-container').superslides('update');
+				$('#page-container').superslides('animate',4);
+				$('#page-container').superslides('stop');
+			}else if(msg.event.payloadData.name == "START" && $('.speech-stat-container').length != 0){
+				$('.speech-stat-container').remove();
+				$('#page-container').superslides('update');
+				$('#page-container').superslides('animate',0);
+				$('#page-container').superslides('start');
+			}
+		}
+	}
+
 
 	// Document on load.
 	$(function(){
@@ -205,9 +263,15 @@
 		burgerMenu();
 		imagePopup();
 		offCanvass();
-                $('#page-container').superslides({
-                    play:0
-                });
+		checkCEPStatus();
+		$('#page-container').superslides({
+			play:0
+		});
+
+		$('#page-container').on('animating.slides',function(){
+			console.log('true')
+		})
+
 	});
 
 
